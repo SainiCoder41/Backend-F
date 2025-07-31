0fadadea7e8cac2fed4ac5c88149fb3649d65f3c
SainiCoder41 <sainicoder41@gmail.com>
2025-07-31 23:07:31 +0530

    New

--- src/controllers/payment.js
+++ src/controllers/payment.js
@@ -33,7 +33,7 @@ paymentRouter.post("/payment", userMiddleware, async (req, res) => {
         },
       ],
       mode: "payment",
-      success_url: `https://www.codexweb.shop/payment/success?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `https://www.codexweb.shop/payment/success?session_id=${CHECKOUT_SESSION_ID}`,
       cancel_url: "https://www.codexweb.shop/payment/cancel",
       customer_email: req?.result?.emailId, // optional
     });
